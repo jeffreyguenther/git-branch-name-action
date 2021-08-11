9860d051a866477dc4bb39e74473bf5bbab1564f
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2021-08-11 11:14:45 -0700

    Update action to remove the leading refs/heads
    We want to preserve the formatting of the branch.

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const core = require("@actions/core")
 
 async function run() {
   try {
-    const isPullRequest = !!process.env.GITHUB_HEAD_REF //GITHUB_HEAD_REF is only set for pull request events https://docs.github.com/en/actions/reference/environment-variables
+    //GITHUB_HEAD_REF is only set for pull request events https://docs.github.com/en/actions/reference/environment-variables
+    const isPullRequest = !!process.env.GITHUB_HEAD_REF 
 
     let branchName
     if (isPullRequest && process.env.GITHUB_HEAD_REF) {
       branchName = process.env.GITHUB_HEAD_REF
     } else {
       if (!process.env.GITHUB_REF) {
-        throw new Error("GITHUB_EVENT_PATH env var not set")
+        throw new Error("GITHUB_REF env var not set")
       }
-      branchName = process.env.GITHUB_REF.split("/")
-        .slice(2)
-        .join("/")
-        .replace(/\//g, "-")
+      branchName = process.env.GITHUB_REF.replace(/refs\/heads\//g, "")
     }
 
     core.exportVariable("GIT_BRANCH_NAME", branchName)
